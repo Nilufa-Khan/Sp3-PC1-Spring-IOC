3350a2d0cc11952610a3d17a684e956f49f2c30d
Nilufa Khan <khannilufa5@gmail.com>
2022-10-31 15:57:37 +0530

    initial

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import org.example.config.AppConfig;
+import org.example.domain.Employee;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Employee employee = context.getBean("emp", Employee.class);
+        System.out.println(employee);
     }
 }
